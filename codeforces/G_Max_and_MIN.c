877b819f3a78192d1ac36e48868ab3d1326eabb9
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-07-18 23:33:07 +0600

    recursive started

--- codeforces/G_Max_and_MIN.c
+++ codeforces/G_Max_and_MIN.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-void findMinMax(int arr[], int size, int *min, int *max) {
-    *min = arr[0]; // Initialize min with the first element
-    *max = arr[0]; // Initialize max with the first element
+// void findMinMax(int arr[], int size, int *min, int *max) {
+//     *min = arr[0]; // Initialize min with the first element
+//     *max = arr[0]; // Initialize max with the first element
 
-    for (int i = 1; i < size; i++) {
-        if (arr[i] < *min) {
-            *min = arr[i];
-        }
+//     for (int i = 1; i < size; i++) {
+//         if (arr[i] < *min) {
+//             *min = arr[i];
+//         }
 
-        if (arr[i] > *max) {
-            *max = arr[i];
-        }
-    }
-}
+//         if (arr[i] > *max) {
+//             *max = arr[i];
+//         }
+//     }
+// }
 
-int main() {
-    int arr[n];
-    scanf("%d",&n);
-    int size = sizeof(arr) / sizeof(arr[0]);
+// int main() {
+//     int arr[n];
+//     scanf("%d",&n);
+//     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int min, max;
-    findMinMax(arr, size, &min, &max);
+//     int min, max;
+//     findMinMax(arr, size, &min, &max);
 
-    printf("Minimum number: %d\n", min);
-    printf("Maximum number: %d\n", max);
+//     printf("Minimum number: %d\n", min);
+//     printf("Maximum number: %d\n", max);
 
-    return 0;
-}
+//     return 0;
+// }
--- codeforces/T_Sort_Numbers.c
+++ codeforces/T_Sort_Numbers.c
@@ -1,28 +1,28 @@
-// #include<stdio.h>
-// void ascending_order(int A,int B,int C) {
-//     if(A>B){
-//         int temp=A;
-//         A=B;
-//         B=temp;
-//     }
-//     if(A>C){
-//         int temp=A;
-//         A=C;
-//         C=temp;
-//     }
-//     if(B>C){
-//         int temp=B;
-//         B=C;
-//         C=temp;
-//     }
-//     printf("%d\n%d\n%d\n",A,B,C);
-// }
-// int main()
-// {
-//     int A,B,C;
-//     scanf("%d %d %d",&A,&B,&C);
-//     ascending_order(A,B,C);
-//     printf("\n");
-//     printf("%d\n%d\n%d\n",A,B,C);
-//     return 0;
-// }
+#include<stdio.h>
+void ascending_order(int A,int B,int C) {
+    if(A>B){
+        int temp=A;
+        A=B;
+        B=temp;
+    }
+    if(A>C){
+        int temp=A;
+        A=C;
+        C=temp;
+    }
+    if(B>C){
+        int temp=B;
+        B=C;
+        C=temp;
+    }
+    printf("%d\n%d\n%d\n",A,B,C);
+}
+int main()
+{
+    int A,B,C;
+    scanf("%d %d %d",&A,&B,&C);
+    ascending_order(A,B,C);
+    printf("\n");
+    printf("%d\n%d\n%d\n",A,B,C);
+    return 0;
+}
