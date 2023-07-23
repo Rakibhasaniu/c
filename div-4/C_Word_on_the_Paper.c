361bfc61689d4cc6ff77bd2f5b34ec1138b2bc77
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-07-23 21:49:02 +0600

    2d array

--- div-4/C_Word_on_the_Paper.c
+++ div-4/C_Word_on_the_Paper.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
+void extract_word(char grid[8][8]) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (grid[j][i] != '.') {
+                putchar(grid[j][i]);
+            }
+        }
+    }
+    putchar('\n');
+}
+
 int main() {
     int t;
     scanf("%d", &t);
+    getchar(); // consume the newline after t
 
-    while (t--) {
+    for (int case_num = 0; case_num < t; case_num++) {
         char grid[8][8];
+
+        // Read the grid for each test case
         for (int i = 0; i < 8; i++) {
-            scanf("%s", grid[i]);
+            for (int j = 0; j < 8; j++) {
+                scanf("%c", &grid[i][j]);
+            }
+            getchar(); // consume the newline after each row
         }
 
-        for (int j = 0; j < 8; j++) {
-            for (int i = 0; i < 8; i++) {
-                if (grid[i][j] >= 'a' && grid[i][j] <= 'z') {
-                    putchar(grid[i][j]);
-                }
-            }
-        }
-        putchar('\n');
+        // Process and print the word for the current test case
+        extract_word(grid);
     }
 
     return 0;
--- div-4/H_The_Third_Letter.c
+++ div-4/H_The_Third_Letter.c
@@ -1,53 +1,34 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-typedef long long ll;
-
-typedef struct {
-    ll x, y;
-} Point;
-
-// Function to check if two points have a valid direction vector
-bool hasValidDirection(Point p1, Point p2) {
-    ll dx = p2.x - p1.x;
-    ll dy = p2.y - p1.y;
-
-    return (dx == 0 || dy == 0 || dx == dy || dx == -dy);
-}
 
 int main() {
-    int t;
-    scanf("%d", &t);
-
-    while (t--) {
-        int n;
-        scanf("%d", &n);
-
-        Point points[n];
-        for (int i = 0; i < n; i++) {
-            scanf("%lld %lld", &points[i].x, &points[i].y);
-        }
-
-        bool found = false;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (!hasValidDirection(points[i], points[j])) {
-                    found = true;
-                    break;
-                }
-            }
-            if (found) {
-                break;
-            }
-        }
-
-        if (found) {
-            printf("NO\n");
-        } else {
-            printf("YES\n");
-        }
+  int t, n, m, ai, bi, di, i;
+  scanf("%d", &t);
+  for (i = 0; i < t; i++) {
+    scanf("%d%d", &n, &m);
+    int camp[n + 1];
+    for (i = 0; i <= n; i++) {
+      camp[i] = 0;
     }
-
-    return 0;
+    for (i = 0; i < m; i++) {
+      scanf("%d%d%d", &ai, &bi, &di);
+      if (di > 0) {
+        camp[ai] = camp[bi] + di;
+      } else {
+        camp[ai] = camp[bi] - di;
+      }
+    }
+    int flag = 1;
+    for (i = 0; i < m; i++) {
+      if (camp[ai] != camp[bi] + di) {
+        flag = 0;
+        break;
+      }
+    }
+    if (flag) {
+      printf("YES\n");
+    } else {
+      printf("NO\n");
+    }
+  }
+  return 0;
 }
