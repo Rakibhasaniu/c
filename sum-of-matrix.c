86d127dc3129b391d2a2d4c2fbe74772aef98e1b
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-07-27 13:57:54 +0600

    sum of marrixes

--- sum-of-matrix.c
+++ sum-of-matrix.c
@@ -18,5 +18,21 @@ int main()
             scanf("%d",&b[i][j]);
         }
     }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+           c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+          printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
