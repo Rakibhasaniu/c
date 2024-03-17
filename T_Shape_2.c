e3015becaaa69bb63469f97f545066307710cfb7
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-03-17 15:38:37 +0600

    hackerrak

--- T_Shape_2.c
+++ T_Shape_2.c
@@ -2,24 +2,19 @@
 
 int main(){
     int n,s,k;
-    scanf("%d",&n);
+    scanf(" %d",&n);
     s=n-1;
     k=1;
-    for(int i=1;i<=(2*n)-1;i++){
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=s;j++){
             printf(" ");
         }
         for(int j=1;j<=k;j++){
             printf("*");
         }
-        if(i<=n-1){
-            s--;
-            k=k+2;
-        } else{
-            s++;
-            k=k-2;
-        }
-        printf( "\n" );
+        s--;
+        k +=2;
+        printf("\n");
     }
     return 0;
 }
---
+++ W_Shape_3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    // Upper part of the diamond
+    for (int i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower part of the diamond
+    for (int i = n - 1; i >= 1; i--) {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
